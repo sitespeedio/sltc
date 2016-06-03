d6ebbf5de5345057da63b213838a4111d516b93e
Sam Smith <git@samsmith.io>
2016-06-03 10:06:56 +0100

    Configure upload/download bandwidth separately
    Co-authored with @beenanner.
    
    The Traffic Control tool (tc) can only classify and police an
    interface's ingress traffic, e.g. dropping packets that exceed some
    rate. However, one of those policing actions is to redirect classified
    traffic to another device. By shaping the egress traffic on the latter
    device, we shape ingress traffic on the former. Use the Intermediate
    Functional Block (IFB) loadable kernel module to create such a device,
    a virtual one.
    
    Note that, currently, we simply shape the traffic on the IFB device as
    we shape the traffic on the physical device.
    
    See sitespeedio/sltc#1, phuedx/sltc#1, and phuedx/sltc#3 for additional
    detail and discussion.
    
    Closes sitespeedio/sltc#1.

--- bin/sltc.js
+++ bin/sltc.js
@@ -8,19 +8,18 @@ var hasbin = require('hasbin'),
 
 var argv = minimist(process.argv.slice(2));
 
-// TODO add different upload and download rate
-// TODO
 if (argv.help) {
     console.log('   Run Simple Linux Traffic Control');
     console.log('   Usage: sltc [options] \n');
     console.log('   Options:');
     console.log('   --device                  The device to use [eth0]');
-    console.log('   --bandwidth               Up and download bandwidth [5Mbps] ');
+    console.log('   --bandwidth.download      Download bandwidth [5Mbps] ');
+    console.log('   --bandwidth.upload        Upload bandwidth [1.6Mbps] ');
     console.log('   --latency                 Latency [28ms]');
     console.log('   --pl                      Packet loss in percent [0%] ');
     console.log('   --remove                  Remove tc current rules');
 } else {
-    if (!hasbin.all.sync(['tc'])) {
+    if (!hasbin.all.sync(['tc', 'modprobe', 'ip'])) {
         console.error('You need to have tc in your path to do traffic control');
     } else {
         sltc(argv);
