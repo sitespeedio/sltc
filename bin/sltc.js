8be5f901a77323ef6ff467a9b726585fd89cdc75
soulgalore <peter@soulgalore.com>
2016-06-16 16:25:14 +0200

    follow airbnb style guide

--- bin/sltc.js
+++ bin/sltc.js
@@ -2,26 +2,26 @@
 
 'use strict';
 
-var hasbin = require('hasbin'),
-    sltc = require('../lib/sltc').sltc,
-    minimist = require('minimist');
+const hasbin = require('hasbin');
+const sltc = require('../lib/sltc').sltc;
+const minimist = require('minimist');
 
-var argv = minimist(process.argv.slice(2));
+const argv = minimist(process.argv.slice(2));
 
 if (argv.help) {
-    console.log('   Run Simple Linux Traffic Control');
-    console.log('   Usage: sltc [options] \n');
-    console.log('   Options:');
-    console.log('   --device                  The device to use [eth0]');
-    console.log('   --bandwidth.download      Download bandwidth [5Mbps] ');
-    console.log('   --bandwidth.upload        Upload bandwidth [1.6Mbps] ');
-    console.log('   --latency                 Latency [28ms]');
-    console.log('   --pl                      Packet loss in percent [0%] ');
-    console.log('   --remove                  Remove tc current rules');
+  console.log('   Run Simple Linux Traffic Control');
+  console.log('   Usage: sltc [options] \n');
+  console.log('   Options:');
+  console.log('   --device                  The device to use [eth0]');
+  console.log('   --bandwidth.download      Download bandwidth [5Mbps] ');
+  console.log('   --bandwidth.upload        Upload bandwidth [1.6Mbps] ');
+  console.log('   --latency                 Latency [28ms]');
+  console.log('   --pl                      Packet loss in percent [0%] ');
+  console.log('   --remove                  Remove tc current rules');
 } else {
-    if (!hasbin.all.sync(['tc', 'modprobe', 'ip'])) {
-        console.error('You need to have tc in your path to do traffic control');
-    } else {
-        sltc(argv);
-    }
+  if (!hasbin.all.sync(['tc', 'modprobe', 'ip'])) {
+    console.error('You need to have tc in your path to do traffic control');
+  } else {
+    sltc(argv);
+  }
 }
