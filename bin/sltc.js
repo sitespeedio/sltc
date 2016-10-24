f9c211ebb24924ef0ba65245940a8182d18a5ccb
soulgalore <peter@soulgalore.com>
2016-10-24 12:33:26 +0200

    rollback to only use straight TC to be able to use inside of Docker without hazzle

--- bin/sltc.js
+++ bin/sltc.js
@@ -13,13 +13,12 @@ if (argv.help) {
   console.log('   Usage: sltc [options] \n');
   console.log('   Options:');
   console.log('   --device                  The device to use [eth0]');
-  console.log('   --bandwidth.download      Download bandwidth [5Mbps] ');
-  console.log('   --bandwidth.upload        Upload bandwidth [1.6Mbps] ');
+  console.log('   --bandwidth               Bandwidth [5Mbps] ');
   console.log('   --latency                 Latency [28ms]');
   console.log('   --pl                      Packet loss in percent [0%] ');
   console.log('   --remove                  Remove tc current rules');
 } else {
-  if (!hasbin.all.sync(['tc', 'modprobe', 'ip'])) {
+  if (!hasbin.all.sync(['tc'])) {
     console.error('You need to have tc in your path to do traffic control');
   } else {
     sltc(argv);
